1ce42afbe767fac7997fa3aafeac8ece5dd28a10
alanoudalshalani <107769859+alanoudalshalani@users.noreply.github.com>
2022-10-04 16:54:56 +0300

    secc

--- hr_pro.py
+++ hr_pro.py
@@ -1,11 +1,39 @@
+
+
+
 class Employee:
+    def __init__(self, name, age, salary, employee_years):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.employee_years = employee_years
+
+        def get_annual_salary(self):
+            return salary*12
+
+
    #Employee class here
 
-class Manager(Employee):
+
+#class Manager(Employee):
+    
+    
+    
+    
+    
+    
+    
     #Manager class here
-        
-def main():
+
+
+
+
+
+
+
+
+#def main():
 	# main code here
 
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+	#main()
